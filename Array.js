57a22f1f4dc5cfa2b17af6a7e9f0793b19c01003
TresnaRani <tresna3122gmail.com>
2022-12-27 20:45:24 +0600

    initial commit

---
+++ Array.js
@@ -0,0 +1,21 @@
+
+
+var names =["rony","jony","banni","tonni"];
+//var names = new Array(6)
+
+/*names[0] ="Tresna"
+names[1] ="robi"
+names[2] ="bobi"
+names[3] ="tikku"
+names[4] ="toma"
+names[5] ="Tresna"
+*/
+
+names.push("pori")
+names.push("topi")
+
+names.pop("[2]")
+
+console.log(names);
+console.log(names.length)
+
