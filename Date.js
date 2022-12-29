dbbc3b05022011253aab92b2e96c1088c3b466a8
TresnaRani <tresna3122gmail.com>
2022-12-29 13:35:33 +0600

    fifth

---
+++ Date.js
@@ -0,0 +1,17 @@
+
+
+var date = new Date();
+
+console.log(date);
+
+var year=date.getFullYear();
+console.log(year);
+
+var day= date.getDay();
+console.log(day)
+
+var month= date.getMonth();
+console.log(month)
+
+var min =date.getMinutes()
+console.log(min)
